2e78901af05f8a2df9c2b292ee9e9834cd7eb30d
Arfan <pathanarfan8@gmail.com>
2026-09-13 17:14:03 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import express from "express"
+import "dotenv/config"
+
+const app = express();
+const Port = process.env.PORT;
+
+app.listen(Port,() => {
+    console.log(`Server Listening on Port ${Port}`)
+})
