8bf531dfd414a40d51a8b738cdee89c028552e98
Arfan <pathanarfan8@gmail.com>
2026-09-13 20:12:43 +0530

    Add Dockerfile and .dockerignore; implement Express server with static file serving

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,12 +6,15 @@ import http from "http";
 import dns from "dns";
 import {clerkMiddleware} from '@clerk/express'
 import cors from "cors"
+import fs from "fs"
+import path from "path";
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const app = express();
 const Port = process.env.PORT;
 const Frontend_Url = process.env.FRONTEND_URL;
+const public_Dir = path.join(process.cwd(),"public");
 
 app.use(express.json());
 app.use(cors({origin:Frontend_Url,credentials:true}));
@@ -20,6 +23,13 @@ app.use(clerkMiddleware());
 app.get("/health", (req,res) => {
     res.status(200).json({ok:true})
 })
+
+if(fs.existsSync(public_Dir)){
+    app.use(express.static(public_Dir))
+    app.get("/{*any}",(req,res,next) => {
+        res.sendFile(path.join(public_Dir,"index.html"),(err) => next(err));
+    })
+}
 
 connectDb();
 
