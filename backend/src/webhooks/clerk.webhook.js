ced8f554eef13466290bf4f5dc70e2b31d8b6484
Arfan <pathanarfan8@gmail.com>
2026-09-16 20:49:25 +0530

    Add Clerk webhook handler to process user events and update database

--- backend/src/webhooks/clerk.webhook.js
+++ backend/src/webhooks/clerk.webhook.js
@@ -1,6 +1,6 @@
 import express from "express"
 import User from "../models/user.model.js"
-import {verifyWebhook} from "@clerk/backend/webhooks"
+import {verifyWebhook} from "@clerk/express"
 
 const router = express.Router();
 
